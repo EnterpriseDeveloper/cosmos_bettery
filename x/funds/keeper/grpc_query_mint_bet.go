42844f797564ff417bccef96f99f6974ebb43ff7
EnterpriseDeveloper <voroshilovmax90@gmail.com>
2021-10-21 17:25:26 +0300

    changed query to get mint query

--- x/funds/keeper/grpc_query_mint_bet.go
+++ x/funds/keeper/grpc_query_mint_bet.go
@@ -6,7 +6,6 @@ import (
 	"github.com/VoroshilovMax/bettery/x/funds/types"
 	"github.com/cosmos/cosmos-sdk/store/prefix"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	"github.com/cosmos/cosmos-sdk/types/query"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
@@ -45,15 +44,31 @@ func (k Keeper) MintBet(c context.Context, req *types.QueryGetMintBetRequest) (*
 		return nil, status.Error(codes.InvalidArgument, "invalid request")
 	}
 
-	var mintBet types.MintBet
+	var mintBets []*types.MintBet
 	ctx := sdk.UnwrapSDKContext(c)
 
-	if !k.HasMintBet(ctx, req.Id) {
-		return nil, sdkerrors.ErrKeyNotFound
+	store := ctx.KVStore(k.storeKey)
+	mintBetStore := prefix.NewStore(store, types.KeyPrefix(types.MintBetKey))
+
+	pageRes, err := query.Paginate(mintBetStore, req.Pagination, func(key []byte, value []byte) error {
+		var mintBet types.MintBet
+		if err := k.cdc.Unmarshal(value, &mintBet); err != nil {
+			return err
+		}
+		m := &mintBet
+		if m.UserId == int64(req.Id) {
+			mintBets = append(mintBets, &mintBet)
+			return nil
+		} else {
+			return nil
+		}
+	})
+
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
 	}
 
-	store := prefix.NewStore(ctx.KVStore(k.storeKey), types.KeyPrefix(types.MintBetKey))
-	k.cdc.MustUnmarshal(store.Get(GetMintBetIDBytes(req.Id)), &mintBet)
+	pageRes.Total = uint64(len(mintBets))
 
-	return &types.QueryGetMintBetResponse{MintBet: &mintBet}, nil
+	return &types.QueryGetMintBetResponse{MintBet: mintBets, Pagination: pageRes}, nil
 }
--- x/funds/types/query.pb.gw.go
+++ x/funds/types/query.pb.gw.go
@@ -123,6 +123,10 @@ func local_request_Query_SwipeBetAll_0(ctx context.Context, marshaler runtime.Ma
 
 }
 
+var (
+	filter_Query_MintBet_0 = &utilities.DoubleArray{Encoding: map[string]int{"id": 0}, Base: []int{1, 1, 0}, Check: []int{0, 1, 2}}
+)
+
 func request_Query_MintBet_0(ctx context.Context, marshaler runtime.Marshaler, client QueryClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq QueryGetMintBetRequest
 	var metadata runtime.ServerMetadata
@@ -143,6 +147,13 @@ func request_Query_MintBet_0(ctx context.Context, marshaler runtime.Marshaler, c
 
 	if err != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "id", err)
+	}
+
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_Query_MintBet_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
 	msg, err := client.MintBet(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
@@ -170,6 +181,13 @@ func local_request_Query_MintBet_0(ctx context.Context, marshaler runtime.Marsha
 
 	if err != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "id", err)
+	}
+
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_Query_MintBet_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
 	msg, err := server.MintBet(ctx, &protoReq)
